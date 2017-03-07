c362ae46fbbb1b01a16abe93a8278e7795025fe8
jona8690 <jona8690@edu.eal.dk>
2017-03-07 08:54:39 +0100

    Started BST

---
+++ LinkedList/MyBST.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList {
+	class MyBST {
+		BNode Root;
+
+		public void Insert(IComparable Data) {
+
+		}
+
+		public int Search(IComparable Data) {
+
+		}
+	}
+
+	class BNode {
+		BNode Left;
+		BNode Right;
+
+		IComparable Data;
+	}
+}
